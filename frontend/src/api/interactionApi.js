8b13592db109d3f18569a309cda3832b016f3de9
Hasmitha Marella <hasmithamarella02@gmail.com>
2026-05-23 16:14:37 +0530

    updated the API Base base URL with render deployed URL

--- frontend/src/api/interactionApi.js
+++ frontend/src/api/interactionApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://ai-crm-backend-xjsq.onrender.com";
 
 export async function saveInteraction(interactionData) {
   const response = await axios.post(
