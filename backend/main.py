3b650cea4ef155ee7b652986ccf2e0e67d62190e
Hasmitha Marella <hasmithamarella02@gmail.com>
2026-05-23 16:33:50 +0530

    update CORS in backend

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,8 @@ Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+                  "https://hcp-crm-ai.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
